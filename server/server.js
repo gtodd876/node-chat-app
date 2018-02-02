2b06c5722c52a9f7b557ef3c2e6aa2f1f64c7ae8
Todd Matthews <gtmatthews@gmail.com>
2018-02-01 20:44:12 -0500

    Fix typo in server js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname + '/../public');
-
+const port = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-//   res.send('index.html');
-// });
-
 app.listen(3000, () => {
-  console.log(`server is servin' all y'all at http://localhost:3000`);
+  console.log(`server is servin' all y'all on ${port}`);
 });
