451ca161ee85aa0290e716fd75f99f657cd7e84b
Todd Matthews <gtmatthews@gmail.com>
2018-02-01 20:36:11 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname + '/../public');
+
+const app = express();
+app.use(express.static(publicPath));
+
+// app.get('/', (req, res) => {
+//   res.send('index.html');
+// });
+
+app.listen(3000, () => {
+  console.log(`server is servin' all y'all at http://localhost:3000`);
+});
